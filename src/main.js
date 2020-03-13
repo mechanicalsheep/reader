89a4a8b3fa4e70d5c009f9a45c1545573b80b33b
mechanicalsheep <shadyakhalil@gmail.com>
2020-03-13 11:40:32 +0300

    Added initial calendar, not optimized
    also had the .native modifier for v-on error ignored

--- src/main.js
+++ src/main.js
@@ -12,8 +12,19 @@ import {firestorePlugin} from 'vuefire'
 
 Vue.config.productionTip = false
 
+
 Vue.use(Toasted)
 Vue.use(firestorePlugin)
+
+const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.';
+Vue.config.warnHandler = function (msg, vm, trace) {
+  // `trace` is the component hierarchy trace
+  if (msg === ignoreWarnMessage) {
+    msg = null;
+    vm = null;
+    trace = null;
+  }
+}
 
 new Vue({
   router,
--- src/views/StudentDashboard.vue
+++ src/views/StudentDashboard.vue
@@ -3,10 +3,14 @@
 <div v-if="student != null">
 
 <h1 v-if="isBirthday">HAPPY BIRTHDAY!!</h1>
-<!-- <h2>Welcome {{student.firstName}} {{student.lastName}}</h2> -->
-<v-row>
-    <v-col cols="3">
-    <v-img :src="student.photoUrl" max-width="150"  ></v-img>
+<h1 style="text-align:center;">Welcome {{student.firstName}} {{student.lastName}}</h1>
+
+<div style="text-align:-webkit-center">
+<v-col cols="6">
+
+<v-row align="center" justify="center">
+    <v-col cols="12">
+    <v-img :src="student.photoUrl" max-width="200px" style="border-radius:90%;"  ></v-img>
     </v-col>
 
     <v-col>
@@ -14,50 +18,75 @@
         <h3>Age: {{age}}</h3>
     </v-col>
 </v-row>
+</v-col>
+</div>
+<v-row>
+<v-col cols="2">
+
+<v-btn fab text small color="grey darken-2" @click="prev">
+              <v-icon small>mdi-chevron-left</v-icon>
+            </v-btn>
+</v-col>
+        <v-col>
+
 <h4 style="text-align:center;">Schedule</h4>
+        </v-col>
+<v-col cols="2">
+
+<v-btn fab text small color="grey darken-2" @click="next">
+              <v-icon small>mdi-chevron-right</v-icon>
+            </v-btn>
+</v-col>
+</v-row>
 </div>
 <v-calendar title="Schedule"
 :events="events"
 :weekdays="weekdays"
+v-model="start"
+ref="calendar"
 >
 
 </v-calendar>
+            
 </v-container>
 </template>
 
 <script>
 import db from '../database'
 import firebase from 'firebase'
+import dateTimeManager from '../scripts/dateTimeManager'
 
 export default{
     name:'StudentDashboard',
     created(){
+        var x = new Date();
+        dateTimeManager.getDayOfTheWeek(x);
+        // console.log(dateTimeManager.formatDate( dateTimeManager.getDateFromString("Wednesday")));
         // this.student=this.$route.params.student;
-        db.collection('Users').doc('dDq45X6MRsT1wR5yPG3fqmgQJ4u2').get()
+        db.collection('Users').doc('lG6qU3bLeRGILxiyBWKC').get()
         .then(userData=>{
             this.student = userData.data();
+            // console.log(this.student.schedule);
             this.name=this.student.firstName +" "+ this.student.lastName;
+            this.getEvents();
         })
-        console.log(this.studentId);
-        console.log(this.student);
+        // console.log(this.studentId);
+        // console.log(this.student);
         // console.log(this.student.photoUrl)
-        this.getEvents();
+        
 
     },
+     mounted () {
+    this.$refs.calendar.checkChange()
+     },
     computed:{
-        getLast: function(){
-        var arr=this.events[this.events.length-1].split('-');
-        var addDays=parseInt(arr[2])+ 7
-        console.log("days is now: "+addDays);
-        arr[2]=addDays.toString();
-        return arr.toString();
-        },
+       
         age: function(){
             try{
 
                 var now = new Date();
                 var dob= new Date(this.student.dob.toDate());
-                console.log((now.getDay()+" "+ (dob.getDay())))
+                // console.log((now.getDay()+" "+ (dob.getDay())))
                 // debugger;
                 if(now.getMonth()+1 == dob.getMonth()+1 && now.getDay()+1 == dob.getDay()){
                     this.isBirthday=true;
@@ -80,23 +109,45 @@ export default{
            events:[
         //    {name: 'test1', start:'2020-03-12', end:'2020-03-13', color:'blue'},
         //    {name: "test2", start:'2020-03-07 12:00'}
-        ]
+        ],
+        start: "2020-03-13"
            
 
         }
     },
     props: ['studentId'],
     methods:{
-        getEvents(day){
-            var date = new Date('2020-03-09');
-            console.log("starting at "+ date)
-            // this.events.push({name: 'something', start:date});
-            for(var i=0; i<100; i++){
-                date=this.addDays(date,7);
-                console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+        prev(){
+            console.log("getting previous")
+            this.$refs.calendar.prev()
+        },
+        next(){
+            console.log("getting next")
+            this.$refs.calendar.next();
+        },
+        getEvents(){
+            console.log(this.$refs.calendar)
+            var createdDate = this.student.createdAt.toDate()
+            console.log("createdAt: "+createdDate)
+           this.student.schedule.forEach(day=>{
+               var dayTime= day.split(' ');
+               var date = dateTimeManager.getDateFromString(dayTime[0], createdDate);
+               this.insertRecurringEvents(date);
+            //    console.log(date);
+           })
+            // var date = new Date('2020-03-09');
+           
+            
+            
+        },
+        insertRecurringEvents(date){
                 this.events.push({name: 'something', start:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`})
-                
-            }
+                for(var i=0; i<100; i++){
+                    date=this.addDays(date,7);
+                    // console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+                    this.events.push({name: 'something', start:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`})
+                    
+                }
         },
     addDays(date, days) {
   var result = new Date(date);
